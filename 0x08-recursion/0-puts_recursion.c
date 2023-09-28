5101d3a9fdd221e811b79968e9d0d0b4ce094ad2
aya-aa <benjaziaaya@hotmail.com>
2023-09-28 05:13:04 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,13 @@
  *@s:input
  *Return:nothing
 */
-
 void _puts_recursion(char *s)
 {
 if (*s)
 {
 	_putchar(*s);
-	_puts_recursion(s+1);
-  }
+	_puts_recursion(s + 1);
+}
 else
 	 _putchar('\n');
 }
