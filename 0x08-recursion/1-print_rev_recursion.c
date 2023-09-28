3abe485e9b77a28101f4c6bd80b68292fd03527b
aya-aa <benjaziaaya@hotmail.com>
2023-09-28 05:38:11 -0700

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_rev_recursion is a recursive function that prints a string in reverse
+ * @s:string to print
+ * return: always 0 success
+*/
+void _print_rev_recursion(char *s)
+{
+if(*s)
+{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+}
