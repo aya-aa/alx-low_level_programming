6085a2f24ca707facb682fd9a0d3621358898f9d
aya-aa <benjaziaaya@hotmail.com>
2023-10-02 12:22:48 -0700

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _sqrt_recursion - return the square root of a number
+* @n : input int
+* Return : the square root of the number or -1 
+*/
+
+
+
+int _sqrt_function( int n, int i)
+{
+if (i * i > n)
+	return (-1);
+else if (i * i == n)
+	return (i);
+else 
+	return(_sqrt_function( n, i+1);
+}
+
+int _sqrt_recursion( int n)
+{
+if (n < -1)
+       return (-1);
+else 
+	retrun(_sqrt_function(n, 0));
+}
